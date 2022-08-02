755297ff9987e6ddf4f69ff0bd0f6a00167d6f58
Thiago Dutra <thiago.dutra@grupoboticario.com.br>
2022-08-02 11:42:28 -0300

    fix: 🪢  exemples

---
+++ modulo1/node-package-json/exemplo2/exemplo2.js
@@ -0,0 +1,3 @@
+console.log(`insira o primeiro valor: ${process.argv[2]}`)
+console.log(`insira o segundo valor: ${process.argv[3]}`)
+console.log(`a soma é: ${Number(process.argv[2])+Number(process.argv[3])}`)
